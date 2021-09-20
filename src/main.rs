e4914beba06bd94bb4860160ca6025d3dd332146
avery.radmacher <avery.radmacher@gmail.com>
2021-09-19 20:25:04 -0400

    Add cli module

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,123 @@
-use domain_types::*;
-use std::process::{Command, ExitStatus, Stdio};
-use std::time::{Duration, Instant};
+mod cli {
+    use crate::core;
+    use crate::domain_types::*;
+    use std::process::ExitStatus;
+    use std::time::Duration;
 
-fn exit_status_to_string(status: ExitStatus) -> String {
-    let code = match status.code() {
-        Some(code) => format!("{}", code),
-        None => String::from("signal"),
-    };
-    let explanation = if status.success() {
-        "success"
-    } else {
-        "failure"
-    };
-    format!("{} ({})", code, explanation)
-}
-
-fn duration_to_string(duration: MsgResult<Duration>, pretty: bool) -> String {
-    if let Err(msg) = duration {
-        return String::from(msg);
+    pub fn parse_args(args: Vec<String>) -> MsgResult<'static, Args> {
+        let mut iter = args.into_iter();
+        let mut display_nanos = false;
+        let mut borrow_stdio = true;
+        let mut command = None;
+        loop {
+            let arg = match iter.next() {
+                None => break,
+                Some(arg) => arg,
+            };
+            if arg == String::from("--nanos") {
+                display_nanos = true;
+            } else if arg == String::from("--hide-stdio") {
+                borrow_stdio = false;
+            } else if arg == String::from("--prog") {
+                command = iter.next();
+                break;
+            } else {
+                command = Some(arg);
+                break;
+            }
+        }
+        let command_args = iter.collect();
+        match command {
+            None => Err("No command specified"),
+            Some(command) => Ok(Args {
+                display_nanos,
+                borrow_stdio,
+                command,
+                command_args,
+            }),
+        }
     }
-    let total_nanos = duration.unwrap().as_nanos();
-    if pretty {
-        let hours = if total_nanos >= 3_600_000_000_000 {
-            format!("{:02}:", total_nanos / 3_600_000_000_000)
-        } else {
-            String::new()
-        };
-        let minutes = if total_nanos >= 60_000_000_000 {
-            format!("{:02}:", total_nanos / 60_000_000_000 % 60)
-        } else {
-            String::new()
-        };
-        let seconds = if total_nanos >= 1_000_000_000 {
-            format!("{:02}", total_nanos / 1_000_000_000 % 60)
-        } else {
-            String::from("0")
-        };
-        let nanos = format!("{:09}", total_nanos % 1_000_000_000);
-        format!("{}{}{}.{}s", hours, minutes, seconds, nanos)
-    } else {
-        format!("{}ns", total_nanos)
-    }
-}
 
-fn print_results(args: &Args, results: ProcessResults) {
-    println!("Results:");
-    println!(
-        "  Exit status: {}",
-        exit_status_to_string(results.exit_status)
-    );
-    println!(
-        "  Duration: {}",
-        duration_to_string(results.duration, !args.display_nanos)
-    );
-    println!();
-}
-
-fn initialize(args: &Args) {
-    let mut command = format!("timit {}", args.command);
-    for arg in &args.command_args {
-        command.push_str(&format!(" {}", arg));
+    fn exit_status_to_string(status: ExitStatus) -> String {
+        let code = match status.code() {
+            Some(code) => format!("{}", code),
+            None => String::from("signal"),
+        };
+        let explanation = if status.success() {
+            "success"
+        } else {
+            "failure"
+        };
+        format!("{} ({})", code, explanation)
     }
-    println!("Command: {}", command);
+
+    fn duration_to_string(duration: MsgResult<Duration>, pretty: bool) -> String {
+        if let Err(msg) = duration {
+            return String::from(msg);
+        }
+        let total_nanos = duration.unwrap().as_nanos();
+        if pretty {
+            let hours = if total_nanos >= 3_600_000_000_000 {
+                format!("{:02}:", total_nanos / 3_600_000_000_000)
+            } else {
+                String::new()
+            };
+            let minutes = if total_nanos >= 60_000_000_000 {
+                format!("{:02}:", total_nanos / 60_000_000_000 % 60)
+            } else {
+                String::new()
+            };
+            let seconds = if total_nanos >= 1_000_000_000 {
+                format!("{:02}", total_nanos / 1_000_000_000 % 60)
+            } else {
+                String::from("0")
+            };
+            let nanos = format!("{:09}", total_nanos % 1_000_000_000);
+            format!("{}{}{}.{}s", hours, minutes, seconds, nanos)
+        } else {
+            format!("{}ns", total_nanos)
+        }
+    }
+
+    fn print_results(args: &Args, results: ProcessResults) {
+        println!("Results:");
+        println!(
+            "  Exit status: {}",
+            exit_status_to_string(results.exit_status)
+        );
+        println!(
+            "  Duration: {}",
+            duration_to_string(results.duration, !args.display_nanos)
+        );
+        println!();
+    }
+
+    fn initialize(args: &Args) {
+        let mut command = format!("timit {}", args.command);
+        for arg in &args.command_args {
+            command.push_str(&format!(" {}", arg));
+        }
+        println!("Command: {}", command);
+    }
+
+    pub fn run(args: Args) {
+        initialize(&args);
+        println!("-- Begin program output --");
+        let results = core::observe_process(&args);
+        println!("--- End program output ---");
+        match results {
+            Ok(results) => print_results(&args, results),
+            Err(reason) => println!("Error: {}", reason),
+        }
+    }
 }
 
 pub mod core {
-    use super::*;
+    use crate::domain_types::*;
+    use std::process::{Command, Stdio};
+    use std::time::Instant;
 
-    fn observe_process(args: &Args) -> MsgResult<ProcessResults> {
+    pub fn observe_process(args: &Args) -> MsgResult<ProcessResults> {
         let mut command = Command::new(&args.command);
         command.args(&args.command_args);
         if !args.borrow_stdio {
@@ -94,17 +144,6 @@ pub mod core {
                 .ok_or("There was an error timing the operation."),
         })
     }
-
-    pub fn run(args: Args) {
-        initialize(&args);
-        println!("-- Begin program output --");
-        let results = observe_process(&args);
-        println!("--- End program output ---");
-        match results {
-            Ok(results) => print_results(&args, results),
-            Err(reason) => println!("Error: {}", reason),
-        }
-    }
 }
 
 pub mod domain_types {
@@ -126,44 +165,10 @@ pub mod domain_types {
     pub type MsgResult<'a, T> = Result<T, &'a str>;
 }
 
-fn parse_args(args: Vec<String>) -> MsgResult<'static, Args> {
-    let mut iter = args.into_iter();
-    let mut display_nanos = false;
-    let mut borrow_stdio = true;
-    let mut command = None;
-    loop {
-        let arg = match iter.next() {
-            None => break,
-            Some(arg) => arg,
-        };
-        if arg == String::from("--nanos") {
-            display_nanos = true;
-        } else if arg == String::from("--hide-stdio") {
-            borrow_stdio = false;
-        } else if arg == String::from("--prog") {
-            command = iter.next();
-            break;
-        } else {
-            command = Some(arg);
-            break;
-        }
-    }
-    let command_args = iter.collect();
-    match command {
-        None => Err("No command specified"),
-        Some(command) => Ok(Args {
-            display_nanos,
-            borrow_stdio,
-            command,
-            command_args,
-        }),
-    }
-}
-
 fn main() {
     let args = std::env::args().skip(1).collect();
-    match parse_args(args) {
+    match cli::parse_args(args) {
         Err(msg) => println!("Error: {}", msg),
-        Ok(args) => core::run(args),
+        Ok(args) => cli::run(args),
     };
 }
