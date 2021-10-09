536270d39e45fe8a645aa4cee76a0e64ab1fe5e6
Avery Radmacher <45777186+avery-radmacher@users.noreply.github.com>
2021-10-09 12:13:49 -0400

    Decompose observe_process() (#11)
    * Add ctrlc dependency
    
    * Add build_command
    
    * Simplify error handling
    
    * Remove ctlrc dependency

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -4,27 +4,26 @@ use std::time::Instant;
 use types::*;
 
 pub fn observe_process(args: &Args, io: IOArgs) -> Result<ProcessData> {
-    let mut command = Command::new(&args.command);
-    command
-        .args(&args.command_args)
-        .stdout(stream_or_null(io.stdout))
-        .stderr(stream_or_null(io.stderr))
-        .stdin(stream_or_null(io.stdin));
+    let mut command = build_command(args, io);
     let start_time = Instant::now();
-    let mut child = match command.spawn() {
-        Ok(child) => child,
-        Err(_) => return Err(Error::NotSpawned),
-    };
-    let exit_status = match child.wait() {
-        Ok(status) => status,
-        Err(_) => return Err(Error::NotJoined),
-    };
+    let mut child = command.spawn().or(Err(Error::NotSpawned))?;
+    let exit_status = child.wait().or(Err(Error::NotJoined))?;
     let end_time = Instant::now();
 
     Ok(ProcessData {
         exit_status,
         duration: end_time.checked_duration_since(start_time),
     })
+}
+
+fn build_command(args: &Args, io: IOArgs) -> Command {
+    let mut command = Command::new(&args.command);
+    command
+        .args(&args.command_args)
+        .stdout(stream_or_null(io.stdout))
+        .stderr(stream_or_null(io.stderr))
+        .stdin(stream_or_null(io.stdin));
+    command
 }
 
 fn stream_or_null(file: Option<std::fs::File>) -> Stdio {
